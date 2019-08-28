b54994eb6371df996e5e568a97bc534cf631b105
Victor <victor.fiamoncini@gmail.com>
2019-08-28 10:42:15 -0300

    Dot env module

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,4 +3,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+// Env:
+import dotenv from 'dotenv'
+dotenv.config()
+
 ReactDOM.render(<App />, document.getElementById('root'))
